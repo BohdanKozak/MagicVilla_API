5fe590b4f5243d715966f9dfa4e250ac2da3b699
Kozak Bohdan <bohdankozak90@gmail.com>
2023-08-31 19:24:31 +0300

    Custom Logger

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Logging;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +12,8 @@ namespace MagicVilla_VillaAPI.Controllers
 
     public class VillaAPIController : ControllerBase
     {
-        private readonly ILogger<VillaAPIController> _logger;
-        public VillaAPIController(ILogger<VillaAPIController> logger)
+        private readonly ILogging _logger;
+        public VillaAPIController(ILogging logger)
         {
             _logger = logger;
         }
@@ -20,7 +21,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            _logger.LogInformation("Getting all villas");
+            _logger.Log("Getting all villas");
             return Ok(VillaStore.VillalIList);
         }
 
@@ -33,13 +34,14 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if (id == 0)
             {
-                _logger.LogError("Get Villa Error with Id: " + id);
+                _logger.Log("Get Villa Error with Id: " + id, "error");
                 return BadRequest();
             }
 
             var villa = VillaStore.VillalIList.FirstOrDefault(u => u.Id == id);
             if (villa == null)
             {
+                _logger.Log("Get Villa Error with Id: " + id, "error");
                 return NotFound();
             }
 
---
+++ MagicVilla_VillaAPI/Logging/Logging.cs
@@ -0,0 +1,33 @@
+﻿namespace MagicVilla_VillaAPI.Logging
+{
+    public class LoggingV2 : ILogging
+    {
+        public void Log(string message, string? type = null)
+        {
+            if (type == "error")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR - " + message);
+                Console.BackgroundColor = ConsoleColor.Black;
+
+            }
+            else
+            {
+                if (type == "warming")
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("ERROR - " + message);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+
+                    Console.WriteLine(message);
+
+                }
+            }
+
+
+        }
+    }
+}
